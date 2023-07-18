e4c3f3fba5384e9068abcc32b16d623367b63dea
pallavidhakne <pmdhakne2002@gmail.com>
2023-07-18 22:19:01 +0530

    Website using react

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Services from './components/Services';
+
+import "./style/App.scss";
+import "./style/Header.scss";
+import "./style/home.scss";
+import "./style/footer.scss";
+import "./style/contact.scss";
+import './style/mediaquery.scss';
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+           <Route path="/" element={<Home />}/>
+           <Route path="/contact" element={<Contact />}/>
+           <Route path="/services" element={<Services />}/>
+      </Routes>
+      <Footer />
+    </Router>
+    
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/pallavi-dhakne-088aa4203/r" target={"blank"}>
+            LinkedIn
+          </a>
+          <a href="https://www.instagram.com/its.pal_20/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/pallavidhakne" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
